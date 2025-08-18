1e38ec098d62a9f48478376873c9db835f20929e
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-18 21:53:27 +0530

    created the modelMapper configuration

---
+++ auth-service/src/main/java/com/company/disaster_response_app/auth_service/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.company.disaster_response_app.auth_service.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
