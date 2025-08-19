574da5ca31012bb71b47e9135b50526adf1e29ce
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-19 08:28:39 +0530

    created the user entity and added the data

---
+++ auth-service/src/main/java/com/company/disaster_response_app/auth_service/entity/User.java
@@ -0,0 +1,29 @@
+package com.company.disaster_response_app.auth_service.entity;
+
+
+import com.company.disaster_response_app.auth_service.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.INDIVIDUAL;
+}
