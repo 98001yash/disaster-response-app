6af3b2c18e1c7bc1f6d052b09a47b1dde1839ef7
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-19 08:21:47 +0530

    created the passwordUtils class to hash and check the password

---
+++ auth-service/src/main/java/com/company/disaster_response_app/auth_service/utils/PasswordUtils.java
@@ -0,0 +1,14 @@
+package com.company.disaster_response_app.auth_service.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtils {
+
+    public String hashPassword(String plainTextPassword){
+        return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String plainTextPassword, String hashedPassword){
+        return BCrypt.checkpw(plainTextPassword, hashedPassword);
+    }
+}
