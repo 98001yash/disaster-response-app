fbbd8696c861e676cc19975cf2941a44914fc733
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-19 21:44:20 +0530

    created the userRepository

---
+++ auth-service/src/main/java/com/company/disaster_response_app/auth_service/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.company.disaster_response_app.auth_service.repository;
+
+import com.company.disaster_response_app.auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
--- auth-service/src/main/java/com/company/disaster_response_app/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/company/disaster_response_app/auth_service/service/AuthService.java
@@ -1,4 +1,14 @@
 package com.company.disaster_response_app.auth_service.service;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class AuthService {
+
+    private final UserRepository userRepository;
 }
