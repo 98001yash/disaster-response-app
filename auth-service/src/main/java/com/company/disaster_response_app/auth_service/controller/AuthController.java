3376e1a24c5146c564b1471f510204e61c8baeb6
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-20 21:56:12 +0530

    created the signup controller

--- auth-service/src/main/java/com/company/disaster_response_app/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/company/disaster_response_app/auth_service/controller/AuthController.java
@@ -1,7 +1,15 @@
 package com.company.disaster_response_app.auth_service.controller;
 
 
+import com.company.disaster_response_app.auth_service.dtos.SignupRequestDto;
+import com.company.disaster_response_app.auth_service.dtos.UserDto;
+import com.company.disaster_response_app.auth_service.entity.User;
+import com.company.disaster_response_app.auth_service.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    
+    private final AuthService authService;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody SignupRequestDto signupRequestDto) {
+        User createdUser = authService.signup(signupRequestDto);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
 }
