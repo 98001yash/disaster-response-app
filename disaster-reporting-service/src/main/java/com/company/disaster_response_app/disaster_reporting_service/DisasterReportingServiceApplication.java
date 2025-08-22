04d933f1d8f1e0e30882011a4ba8cf807677f049
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-22 09:54:33 +0530

    added  the disataer-reporting-service

---
+++ disaster-reporting-service/src/main/java/com/company/disaster_response_app/disaster_reporting_service/DisasterReportingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.disaster_response_app.disaster_reporting_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterReportingServiceApplication.class, args);
+	}
+
+}
---
+++ disaster-reporting-service/src/test/java/com/company/disaster_response_app/disaster_reporting_service/DisasterReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.disaster_response_app.disaster_reporting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
