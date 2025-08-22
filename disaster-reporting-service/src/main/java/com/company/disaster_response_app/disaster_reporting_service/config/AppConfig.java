9f993721f87a970696ea36e5181ba77fe8d843c2
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-22 10:27:51 +0530

    a

---
+++ disaster-reporting-service/src/main/java/com/company/disaster_response_app/disaster_reporting_service/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.company.disaster_response_app.disaster_reporting_service.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppConfig {
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+
+    }
+}
